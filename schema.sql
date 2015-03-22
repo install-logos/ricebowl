2b4aaa542a64746244e605864a5586fd8c11d01f
Ricky H <rickylqhan@gmail.com>
2015-03-22 15:17:52 -0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -8,8 +8,8 @@ CREATE TABLE packages (
   images TEXT NOT NULL,
   description TEXT NOT NULL
 );
-INSERT INTO "packages" VALUES(1,'linux repo 1','i3','http://bwasti.com:9001/test.zip','[''http://bwasti.com:9001/test.jpg'']','This is a sample of the description field.');
-INSERT INTO "packages" VALUES(2,'linux repo 2','i3','http://bwasti.com:9001/test.zip','[''http://bwasti.com:9001/test2.jpg'']','Does this description thing work?.');
-INSERT INTO "packages" VALUES(3,'linux repo 3','i3','http://bwasti.com:9001/test.zip','[''http://bwasti.com:9001/test3.png'']','test.');
-INSERT INTO "packages" VALUES(4,'logos','i3','http://penisland.com','sth','your server is working. Congrats...');
+INSERT INTO "packages" VALUES(1,'linux repo 1','i3','http://bwasti.com:9001/test.zip','["http://bwasti.com:9001/test.jpg"]','This is a sample of the description field.');
+INSERT INTO "packages" VALUES(2,'linux repo 2','i3','http://bwasti.com:9001/test.zip','["http://bwasti.com:9001/test2.jpg"]','Does this description thing work?.');
+INSERT INTO "packages" VALUES(3,'linux repo 3','i3','http://bwasti.com:9001/test.zip','["http://bwasti.com:9001/test3.png"]','test.');
+INSERT INTO "packages" VALUES(4,'logos','i3','http://penisland.com','["http://bwasti.com:9001/test3.png"]','your server is working. Congrats...');
 COMMIT;
