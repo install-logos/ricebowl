5299f99827fa54e37c0711a1fd98d9bac294c75b
Ricky Han <rickylqhan@gmail.com>
2015-03-21 22:01:20 -0400

    added manager script

---
+++ manage.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from flask.ext.script import Manager, Server
+from ricedb_server import app
+
+manager = Manager(app)
+manager.add_command("runserver", Server(
+    use_debugger = True,
+    use_reloader = True,
+    host = '0.0.0.0')
+)
+
+if __name__ == "__main__":
+    manager.run()
