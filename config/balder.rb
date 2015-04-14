594798c9947361906deb60b7bddba7483b330474
Ricky Han <rickylqhan@gmail.com>
2015-04-13 22:15:56 -0400

    edit config

--- config/balder.rb
+++ config/balder.rb
@@ -1,7 +1,7 @@
 ENV['STORAGE_PATH']       = "uploads" if ENV['STORAGE_PATH'].nil?
 #ENV['PRIVATE']           = 'true'
-#ENV['TITLE']            = 'photo'
-#ENV['HEROKU']           = 'true'
+ENV['TITLE']            = 'Ricebowl'
+ENV['HEROKU']           = 'false'
 #ENV['S3_KEY']           = ''
 #ENV['S3_SECRET']        = ''
 #ENV['S3_BUCKET']        = ''
